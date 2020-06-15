3c447fc7feb00d77b6440d7ca5cc6f3abde9cf4a
EvaMaeRey <evangeline.mae@gmail.com>
2020-06-15 17:50:49 -0600

    adding the webshot

--- R/visual_table_of_contents.R
+++ R/visual_table_of_contents.R
@@ -1,3 +1,32 @@
+#### take webshots of flipbooks #####
+
+
+flipbook_take_webshots <- function(base_url =
+                         "https://evamaerey.github.io/data_manipulation/one_stream_wrangle.html",
+                       dir = "test",
+                       num_pages,
+                       pad_digits = 0) {
+
+  if (!dir.exists(dir)) {dir.create(dir)}
+
+  for (i in 1:num_pages) {
+
+    url <- paste0(base_url, "#", i)
+    i_padded <- stringr::str_pad(i, 2, pad = "0")
+    file <- paste0(dir, "/webshot", i_padded, ".png")
+    webshot::webshot(url = url, file = file)
+
+    i <- i + 1
+
+  }
+
+}
+
+# flipbook_take_webshots(num_pages = 22)
+
+
+
+
 ##### Build gif #####
 
 build_gif <- function(dir,
@@ -124,11 +153,71 @@ build_and_use_gif <- function(href,
 
 }
 
-knitr::opts_chunk$set(echo = F, eval = T, message = F, warning = F, cache = F)
+
+#' Title
+#'
+#' @param dir
+#' @param base_url
+#' @param href
+#' @param num_pages
+#' @param pattern
+#' @param which
+#' @param width
+#' @param height
+#' @param delay
+#' @param static_file
+#' @param gif_file
+#' @param num_in_gif
+#' @param cached_gif
+#'
+#' @return
+#' @export
+#'
+#' @examples
+flipbook_webshots_build_and_use_gif <- function(dir,
+                              base_url,
+                              href = base_url,
+                              num_pages,
+                              pattern = "",
+                              which = 5,
+                              width = 100,
+                              height = 100,
+                              delay = .2,
+                              static_file = select_static(dir, pattern, which),
+                              gif_file = paste0(dir, "/viztoc_", pattern, ".gif"),
+                              num_in_gif = NULL,
+                              cached_gif = TRUE
+                              ){
+
+  if (cached_gif == FALSE | !file.exists(gif_file)) {
+
+    flipbook_take_webshots(dir = dir,
+                           base_url = base_url,
+                           num_pages = num_pages)
+
+    build_gif(dir = dir,
+              pattern = "",
+              gif_file = gif_file,
+              num_in_gif = num_in_gif,
+              width = width,
+              height = height,
+              delay = delay)
+
+  }
+
+  create_html_use_static_and_gif(href = href,
+                                 static_file = static_file,
+                                 gif_file = gif_file,
+                                 width = width,
+                                 height = height)
+
+}
 
 
-
-
+# flipbook_webshots_build_and_use_gif(dir = "hi/",
+#                                     num_pages = 22,
+#                                     pattern = "",
+#                                     base_url = "https://evamaerey.github.io/tidyverse_in_action/one_stream_wrangle.html")
 
 
 #### Save plot from chunk ##########
